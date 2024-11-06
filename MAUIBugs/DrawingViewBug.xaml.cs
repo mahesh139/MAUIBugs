301d4fca87be8966f2999a7ae2a7c5ad67a984e5
Mahesh Thakkar <thakkar.mh@gmail.com>
2024-11-06 12:26:16 +0530

    Add project files.

---
+++ MAUIBugs/DrawingViewBug.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MAUIBugs.ViewModels;
+
+namespace MAUIBugs
+{
+    public partial class DrawingViewBug : ContentPage
+    {
+        public DrawingViewBug()
+        {
+            InitializeComponent();
+            this.BindingContext = new TestViewModel();
+        }
+    }
+}
